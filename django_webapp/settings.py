7e2ba095ec68a0cd1ad1dc415c19f337243ceb62
alexHooper <alexmohack@gmail.com>
2018-07-28 22:23:34 +0530

    add heroku domain in allowed hosts

--- django_webapp/settings.py
+++ django_webapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secret_key.read()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pacific-brushlands-76325.herokuapp.com']
 
 
 # Application definition
