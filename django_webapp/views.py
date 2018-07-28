c8d8e05be540b15b324ddf0b7f8eb47e99788643
alexHooper <alexmohack@gmail.com>
2018-07-28 17:27:08 +0530

    change templates path

--- django_webapp/views.py
+++ django_webapp/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def home_page(request):
-	return render(request, 'home_page.html')
+	return render(request, 'bootstrap/home_page.html')
 
 
 def features_page(request):
-	return render(request, 'feature_page.html')
+	return render(request, 'bootstrap/feature_page.html')
 
 
 def pricing_page(request):
-	return render(request, 'pricing_page.html')
+	return render(request, 'bootstrap/pricing_page.html')
 
 
 def about_page(request):
-	return render(request, 'about_page.html')
+	return render(request, 'bootstrap/about_page.html')
